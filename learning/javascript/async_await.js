c271113d84fa3b4ce0543873b34281ea01dbe335
Patrick Raco <pataraco@gmail.com>
2019-12-06 00:39:29 -0800

    add async example

--- learning/javascript/async_await.js
+++ learning/javascript/async_await.js
@@ -1,6 +1,5 @@
 'use strict'  // forces strict type setting
 
-// --- promises ---
 function square(data) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -13,20 +12,7 @@ function square(data) {
     });
 }
 
-square(8).then(
-    (result) => {
-        console.log('square(8)[resolve] result: ' + result); },
-    (errmsg) => {
-        console.log('square(8)[reject] error: ' + errmsg); }
-)
-
-square('eight').then(
-    (result) => {
-        console.log('square(\'eight\')[resolve] result: ' + result); },
-    (errmsg) => {
-        console.log('square(\'eight\')[reject] errmsg: ' + errmsg); }
-)
-
+// --- promises ---
 square(2).then(result => {
     console.log('square(2)[resolve] result: ' + result);
     return square(result);
@@ -38,4 +24,34 @@ square(2).then(result => {
     return square('result');
 }).catch(errmsg => {
     console.log('square[catch] errmsg: ' + errmsg);
+});
+
+async function chainSquare(x) {  // async functions always return a promise
+    // --- return => resolve ---
+    // let result = await square(x);
+    // return result;
+
+    // --- errors => reject ---
+    // return await square('a'); // error: data is not a number
+    // return await square(a);   // ReferenceError: a is not defined
+
+    // --- one-liner ---
+    // return await square(data);
+
+    // --- chain --- (any errors => reject)
+    console.log('in chainSquare: x=' + x);
+    let a = await square(x);
+    console.log('in chainSquare: a=' + a);
+    let b = await square(a);
+    console.log('in chainSquare: b=' + b);
+    let c = await square(b);
+    console.log('in chainSquare: c=' + c);
+    return c;
+}
+
+var x = 2;
+chainSquare(x).then(result => {
+    console.log('chainSquare(x)[resolve] result: ' + result);
+}).catch(errmsg => {
+    console.log('chainSquare(x)[catch] errmsg: ' + errmsg);
 });
