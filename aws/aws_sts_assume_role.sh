e041d1b2c61dc7e1bfe9bdc40c1f73408f752d71
Patrick Raco <pataraco@gmail.com>
2020-01-03 20:27:03 -0800

    remove 'jq' requirement and improve

--- aws/aws_sts_assume_role.sh
+++ aws/aws_sts_assume_role.sh
@@ -10,7 +10,6 @@
 # Requirements:
 #   Must have AWS credentials configured (i.e. environment variables set)
 #   Must use [profile $#PROFILE] in ~/.aws/config file for your profiles
-#   Must have 'aws' and 'jq' installed
 #
 #   Example ~/.aws/config
 #
@@ -56,14 +55,14 @@ if [ -n "$PROFILE" ]; then
       unset AWS_SECURITY_TOKEN
       role_arn=$(awk '$2~/'"$PROFILE"']/ {pfound="true"; next}; (pfound=="true" && $1~/role_arn/) {print $3; exit}; (pfound=="true" && $1~/profile/) {exit}' $AWS_CFG | sed 's/ *$//')
       role_name=$(cut -d'/' -f2- <<< $role_arn)
+      aws_acct=$(cut -d':' -f5 <<< $role_arn)
       aws sts assume-role --role-arn $role_arn --role-session-name $PROFILE --duration-seconds $STS_DURATION > $AWS_STS_CREDS
       if [ $? -eq 0 ]; then
-         export AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId $AWS_STS_CREDS)
-         export AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey $AWS_STS_CREDS)
-         export AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken $AWS_STS_CREDS)
+         export AWS_ACCESS_KEY_ID=$(grep AccessKeyId $AWS_STS_CREDS | cut -d'"' -f4)
+         export AWS_SECRET_ACCESS_KEY=$(grep SecretAccessKey $AWS_STS_CREDS | cut -d'"' -f4)
+         export AWS_SESSION_TOKEN=$(grep SessionToken $AWS_STS_CREDS | cut -d'"' -f4)
          aws_default_region=$(awk '$2~/'"$PROFILE"']/ {pfound="true"; next}; (pfound=="true" && $1~/region/) {print $NF; exit}; (pfound=="true" && $1~/profile/) {exit}' $AWS_CFG)
          [ -n aws_default_region ] && export AWS_DEFAULT_REGION=$aws_default_region
-         aws_acct=$(aws sts get-caller-identity | jq -r .Account)
          echo "role has been assumed: $aws_acct ($role_name)"
       fi
    fi
